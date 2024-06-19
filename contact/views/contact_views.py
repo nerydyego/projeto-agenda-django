2eb1a25e47e76dcdfc76be9a5a7353b19e8341a6
nerydyego <dyegonery@gmail.com>
2024-06-19 06:10:33 -0300

    Criação de paginação no search (views, template)

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
-from django.http import Http404
+from django.core.paginator import Paginator
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    # Paginação django "https://docs.djangoproject.com/pt-br/5.0/topics/pagination/"
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
     return render(
@@ -30,9 +34,14 @@ def search(request):
             )\
         .order_by('-id')
     
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
+        'search_value': search_value,
     }
     return render(
         request,
